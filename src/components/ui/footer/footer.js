f390ed0758576d31165f172c66549cbf3f877e73
Matt Cole <mcole.uk@gmail.com>
2023-09-02 08:54:09 +0100

    Changed Date to 2023

--- src/components/ui/footer/footer.js
+++ src/components/ui/footer/footer.js
@@ -5,7 +5,7 @@ const footer = () => (
         <footer className='py-3 my-4'>
             <div className='nav justify-content-center border-bottom pb-3 mb-3' />
             
-            <p className='text-center text-muted'>© 2022 Manchester Metrolink, Ltd</p>
+            <p className='text-center text-muted'>© 2023 Manchester Metrolink, Ltd</p>
         </footer>
     </div>
 );
